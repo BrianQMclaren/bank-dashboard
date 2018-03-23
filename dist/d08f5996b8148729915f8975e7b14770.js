3615fe78381f950749c24fd3bf829c232ffdfa90
Brian McLaren <brianqmclaren@gmail.com>
2018-03-22 21:47:17 -0400

    Added svg icons to sidebar

--- dist/d08f5996b8148729915f8975e7b14770.js
+++ dist/d08f5996b8148729915f8975e7b14770.js
@@ -150,7 +150,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://' + window.location.hostname + ':57145/');
+  var ws = new WebSocket('ws://' + window.location.hostname + ':51533/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
