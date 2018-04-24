753415653b6fd1f99941001a7c2c3a9ab3bbb21c
Brian McLaren <brianqmclaren@gmail.com>
2018-04-24 16:34:05 -0400

    experimenting with css grid layout

--- dist/app.e623f3da.js
+++ dist/app.e623f3da.js
@@ -109,7 +109,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50629' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '62383' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
