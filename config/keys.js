f1a42c0ddc6b39721b22506fa28e9fc6fba69861
Brian McLaren <brianqmclaren@gmail.com>
2019-07-30 15:12:13 -0400

    connect mongodb

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-   "mongodb://heroku_fqv9hg2h:Quin45ton!@ds131737.mlab.com:31737/unibank",
+  mongoURI: "mongodb://client:client123@ds131737.mlab.com:31737/unibank",
   secretOrKey: "secret"
 };
