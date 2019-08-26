5e8e39dfaa82410324bca9c5dffc4facad72fafc
Brian McLaren <brianqmclaren@gmail.com>
2019-08-26 13:56:36 -0400

    Add heroku build script

--- server.js
+++ server.js
@@ -34,6 +34,14 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/plaid", plaid);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
